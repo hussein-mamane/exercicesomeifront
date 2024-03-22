550a222e874344241af1481d4cff834fd4a6ae7c
hussein_mamane <hous.mamane012@gmail.com@>
2024-03-22 15:26:44 +0000

    must add detail navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {ProductListComponent} from "./components/product-list/product-list.component";
+import {ProductDetailComponent} from "./components/product-detail/product-detail.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  /*{
+    path:'',
+    redirectTo:'list',
+    pathMatch:'full'
+  },*/
+  {
+    path:'',
+    component:ProductListComponent
+  },
+  {
+    path:'list/:id',
+    component:ProductDetailComponent,
+    pathMatch:'full'
+  },
+];
